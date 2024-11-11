96f4dd15eb60ab05a90a4b99a56178321521a0df
Tunde Balogun <tuns4fun@gmail.com>
2024-11-11 19:25:22 +0100

    Added Python Week1 Assignment

---
+++ Assignment-week1/user_input.py
@@ -0,0 +1,5 @@
+name = input("Please input your name: ");
+age = input("Please input your age: ");
+location = input("Please insert your location: ");
+
+print(f"Hello {name}, you are {age}years old and live in {location}");
